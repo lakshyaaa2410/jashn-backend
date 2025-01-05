b77e2eab6ba6792158890522a708c6b75dc44f22
Lakshya Choudhary <lakshyachoudhary1@gmail.com>
2025-01-05 23:02:29 +0530

    Security Middlewares Added

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const express = require("express");
 const eventRouter = require("./routes/eventRoutes");
 const userRouter = require("./routes/userRoutes");
+
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+
 const app = express();
+
+// Middleware For Sending HTTPS Response Headers
+app.use(helmet());
+
+// Rate Limiting Middelware
+const limit = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: "Too Many Requests From This IP, Please Try Again!",
+});
+app.use("/api", limit);
+
+// Middleware For Data Sanitization Against NoSQL Query Injection
+app.use(mongoSanitize());
+
+// Middleware For Data Sanitization Against XSS
+app.use(xss());
+
+// Middleware For Controlling Paramter Population
+app.use(hpp());
 
 app.use(express.json());
 
